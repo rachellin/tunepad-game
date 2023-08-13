0082e9827396e16a614caf5301ed0d36c017ff63
rachellin <rachlin232@gmail.com>
2023-08-13 12:32:19 -0500

    initial commit

---
+++ src/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Container extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>hi</div>
+        )
+    }
+
+}
